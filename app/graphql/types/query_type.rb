9b9100eac0d60d4be9072cd35bd2fd246e683438
patryk-mrozinski <patryk.mrozinski.pm@gmail.com>
2024-08-21 21:31:50 +0200

    add boat query type and remove test_field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -18,14 +18,12 @@ module Types
       ids.map { |id| context.schema.object_from_id(id, context) }
     end
 
+    field :boats, [Types::BoatType], null: false
+    def boats
+      Boat.all
+    end
+
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: 'An example field added by the generator'
-    def test_field
-      'Hello World!'
-    end
   end
 end
