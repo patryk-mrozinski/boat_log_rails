f2ef3ed8e773b8040f75e8b439bd23d2638fb30e
patryk-mrozinski <patryk.mrozinski.pm@gmail.com>
2024-08-26 22:45:29 +0200

    clean up spec

--- spec/graphql/mutations/users/sign_in_spec.rb
+++ spec/graphql/mutations/users/sign_in_spec.rb
@@ -2,69 +2,70 @@
 
 require 'rails_helper'
 
-describe Mutations::Users::SignIn, type: :request do
+RSpec.describe Mutations::Users::SignIn, type: :request do
   let(:password) { 'password' }
   let(:email) { Faker::Internet.email }
   let!(:user) { create(:user, password:, email:) }
-  let(:respose) {  }
-
   let(:query_context) { {} }
   let(:query_variables) do
     {
-      email:    email,
+      email: email,
       password: password
     }
   end
+  let(:expected_error) { 'Invalid credentials' }
 
-  context 'when valid' do
-    it 'returns token and response information' do
-      response = graphql!['data']['signIn']
-
-      expect(response['success']).to eq(true)
-      expect(response['token']).to be_kind_of(String)
-    end
-
-    it 'returns correct user information' do
-      user_response = graphql!['data']['signIn']['user']
-
-      expect(user_response['email']).to eq(email)
-      expect(user_response['lastName']).to eq(user.last_name)
-    end
+  def graphql_response
+    graphql!['data']['signIn']
   end
 
-  context 'when invalid' do
-    let(:expected_error) { 'Invalid credentials' }
+  describe 'SignIn Mutation' do
+    context 'when valid' do
+      it 'returns token and success' do
+        response = graphql_response
 
-    context 'with invalid email' do
-      let!(:user) { create(:user, password:, email: Faker::Internet.email) }
+        expect(response['success']).to be(true)
+        expect(response['token']).to be_a(String)
+      end
 
-      it 'returns correct response' do
-        response = graphql!['data']['signIn']
+      it 'returns correct user information' do
+        user_response = graphql_response['user']
 
-        expect(response['success']).to eq(false)
-        expect(response['token']).to be_nil
-        expect(response['user']).to be_nil
-        expect(response['error']).to eq(expected_error)
+        expect(user_response['email']).to eq(email)
+        expect(user_response['lastName']).to eq(user.last_name)
       end
     end
 
-    context 'with invalid password' do
-      let!(:user) { create(:user, password: Faker::Internet.password, email:) }
-      it 'returns correct response' do
-        response = graphql!['data']['signIn']
+    context 'when invalid' do
+      shared_examples 'an invalid sign-in' do
+        it 'returns error message' do
+          response = graphql_response
 
-        expect(response['success']).to eq(false)
-        expect(response['token']).to be_nil
-        expect(response['user']).to be_nil
-        expect(response['error']).to eq(expected_error)
+          expect(response['success']).to be(false)
+          expect(response['token']).to be_nil
+          expect(response['user']).to be_nil
+          expect(response['error']).to eq(expected_error)
+        end
+      end
+
+      context 'with invalid email' do
+        let!(:user) { create(:user, password:, email: Faker::Internet.email) }
+
+        it_behaves_like 'an invalid sign-in'
+      end
+
+      context 'with invalid password' do
+        let!(:user) { create(:user, password: Faker::Internet.password, email:) }
+
+        it_behaves_like 'an invalid sign-in'
       end
     end
   end
 
   def query
     <<~GQL
-      mutation login($email: String!, $password: String!){
-        signIn(input: { email: $email, password: $password}) {
+      mutation login($email: String!, $password: String!) {
+        signIn(input: { email: $email, password: $password }) {
           user {
             email
             id
